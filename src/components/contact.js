d865e6157a54b125700a548766b21f2cee762ee2
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2022-10-08 17:14:19 -0500

    the styling for the contacts needs work

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,10 +7,10 @@ import BitMojiHi from './../images/BitMojiHi.png';
 class Contact extends Component {
     render(){
         return(
-            <div className='contact-body'>
+            <div className='contact-body' style={{overflow:'auto'}}>
                  <Grid className="contact-grid">
                  {/* 2 Cells with location of 6 column, the lower the number in the column, the smaller the Cell?. */}
-                    <Cell col={6}><h1>Brijido Ortiz</h1>
+                    <Cell col={4}><h1>Brijido Ortiz</h1>
                         <img src={BitMojiHi}
                             alt="BitMojiHi"
                             style={{height: '250px'}}
@@ -18,25 +18,26 @@ class Contact extends Component {
                             <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}> hello this is Brijido Ortiz</p>
                     </Cell>
                    
-                    <Cell col={6}>
+                    <Cell col={8}>
                         <h2>Contact me</h2>
                             <hr/>
                             <div className='contact-list'>
-                            <List>
+                            <List style={{}}>
                                 <ListItem>
                                 <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
-                                <i className='fa fa-phone-square' aria-hidden="true"/>  309-299-2589
+                              <span style={{display: 'flex', justifyContent: 'flex-start', flewWrap:'wrap', alignItems: 'flex-end'}}>  
+                              <i className='fa fa-phone-square' aria-hidden="true"/>  309-299-2589</span>
                               
                                 </ListItemContent>
                                 </ListItem>
                                 <ListItem>
                             <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
-                                <i className='fa fa-snapchat' aria-hidden="true"/> bortiz4ever
+                            <span style={{display: 'flex', justifyContent: 'flex-start', flexWrap:'wrap', alignItems:'flex-end'}}><i className='fa fa-snapchat' aria-hidden="true"/> bortiz4ever</span>
                                 </ListItemContent>
                                 </ListItem>
                                 <ListItem>
-                                 <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton', flexGrow: 3}}>
-                                 <span style={{display: 'flex', justifyContent:'flex-start', flexWrap: 'wrap'}}><i className='fa fa-envelope' aria-hidden='true'/>
+                                 <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                 <span style={{display: 'flex', justifyContent:'flex-start', flexWrap: 'wrap', alignItems:'flex-end'}}><i className='fa fa-envelope' aria-hidden='true'/>
                                 Bortiz4ever@gmail.com</span>
                 
                                 </ListItemContent>    
